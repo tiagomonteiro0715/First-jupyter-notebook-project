08407ae2d234ae9f99f529fd813fa138ea3fcdf5
Tiago monteiro <tiago.monteiro0715@gmail.com>
2020-08-15 19:12:55 +0100

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,3 @@
+This project started on 15-08-2020
+
+this project has the objective exploring the usage of 3d graphs and 2d special graphs
---
+++ main.py
@@ -0,0 +1,24 @@
+import matplotlib.pyplot as mpl
+import numpy as np
+import pandas as pd
+
+df = pd.read_csv('diamonds.csv')
+
+
+
+
+
+
+
+
+
+
+
+
+def main():
+    return 0
+
+
+
+if __name__ == "__main__":
+    main()
